66db280a52e619c7d9fd35024f789d31660b5b43
rsboarder <rsboarder@gmail.com>
2015-09-06 15:23:59 +0300

    ~ pass to jade all options

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 /*
-	MIT License http://www.opensource.org/licenses/mit-license.php
-	Author Scott Beck @bline
-*/
+ MIT License http://www.opensource.org/licenses/mit-license.php
+ Author Scott Beck @bline
+ */
 var loaderUtils = require("loader-utils");
-module.exports = function(source) {
-	this.cacheable && this.cacheable(true);
-	var jade = require("jade");
-	var query = loaderUtils.parseQuery(this.query);
-	var req = loaderUtils.getRemainingRequest(this).replace(/^!/, "");
-	var tmplFunc = jade.compile(source, {
-		filename: req,
-		self: query.self,
-		pretty: query.pretty,
-		locals: query.locals,
-		doctype: query.doctype || 'html',
-		compileDebug: this.debug || false
-	});
 
-	return tmplFunc(query);
+function extend() {
+    var target = {};
+
+    for (var i = 0; i < arguments.length; i++) {
+        var source = arguments[i];
+
+        for (var key in source) {
+            if (source.hasOwnProperty(key)) {
+                target[key] = source[key];
+            }
+        }
+    }
+
+    return target
+}
+
+module.exports = function (source) {
+    this.cacheable && this.cacheable(true);
+    var jade = require("jade");
+    var query = loaderUtils.parseQuery(this.query);
+    var req = loaderUtils.getRemainingRequest(this).replace(/^!/, "");
+    var opts = extend({}, {filename: req}, query);
+
+    if (!opts.doctype) {
+        opts.doctype = 'html';
+    }
+
+    var tmplFunc = jade.compile(source, extend(opts, {compileDebug: this.debug || false}));
+
+    return tmplFunc(query.locals);
 }
